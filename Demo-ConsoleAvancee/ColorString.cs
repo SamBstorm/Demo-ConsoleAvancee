e147969e4437f266548d73c1163a27f9fa5752e5
Samuel Legrain <samuel.legrain@bstorm.be>
2024-02-06 10:12:56 +0100

    Ajoutez des fichiers projet.

---
+++ Demo-ConsoleAvancee/ColorString.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_ConsoleAvancee
+{
+    internal struct ColorString
+    {
+        public string text { get; set; }
+
+        public Dictionary<int, ConsoleColor> colors { get; set; }
+
+        public void Writeline()
+        {
+            for (int i = 0; i < text.Length; i++) { 
+                if(colors.ContainsKey(i))
+                {
+                    Console.ForegroundColor = colors[i];
+                }
+                Console.Write(text[i]);
+            }
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+
+        public void Writeline(int x, int y, int width)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (colors.ContainsKey(i))
+                {
+                    Console.ForegroundColor = colors[i];
+                }
+                Console.SetCursorPosition(x + (i % width), y + (i / width));
+                Console.Write(text[i]);
+            }
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Demo-ConsoleAvancee/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Demo_ConsoleAvancee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            ColorString colorString = new ColorString();
+
+            colorString.text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, labore. Ullam 😉 quam at repellendus magnam. Sunt cum nulla accusamus nostrum. Sequi iure atque modi. Atque voluptas exercitationem impedit error obcaecati. 🔥🍃🚿";
+            colorString.colors = new Dictionary<int, ConsoleColor>
+            {
+                {71, ConsoleColor.Green},
+                {76, ConsoleColor.Gray}
+            };
+            Console.WriteLine(colorString.text);
+
+            colorString.Writeline(Console.WindowWidth/2-20,3,60);
+            Thread.Sleep(2000);
+            Console.WriteLine("🔻");
+            Console.ReadLine();
+            colorString.Writeline();
+
+            Console.WriteLine(
+@"/-----\
+| O O |
+\__-__/"
+);
+            string[] words = colorString.text.Split(new char[] { ' ', '.', ',',';' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
